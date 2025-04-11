5dc8946e3f3850d6f5aec95ebbde91f11ca16870
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2025-04-11 15:38:55 +0200

    creation Banner.jsx + Error.jsx

--- Frontend/src/pages/home/home.jsx
+++ Frontend/src/pages/home/home.jsx
@@ -1,1 +1,25 @@
 import React from "react";
+import iconChat from "../../assets/icon-chat.webp"
+import iconMoney from "../../assets/icon-money.webp"
+import iconSecurity from "../../assets/icon-security.webp"
+import texts from "../../data/dataText.json";
+
+
+function Home() {
+    const imageFeature = {
+        "icon-chat.webp": iconChat,
+        "icon-money.webp": iconMoney,
+        "icon-security.webp": iconSecurity,
+    }
+
+    return (
+        <main>
+            <Banner
+            titleOne={texts.titleOne}
+            titleTwo={texts.titleTwo}
+            text={texts.text}
+            alt={texts.alt}
+            />
+        </main>
+    )
+}
