9f024e4430709898c9f7b2a7e7fb22ada475e181
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2025-04-11 14:52:01 +0200

    creation des fichiers json dans src/data

--- Frontend/src/pages/error/error.jsx
+++ Frontend/src/pages/error/error.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+
+
+const Error = () => {
+    return (
+        <main className= 'error'>
+            <section>
+                <h2> ERROR 404</h2>
+                <p>Page not found</p>
+                <Navlink to='/'>
+                    <h3>Back to home Page</h3>
+                </Navlink>
+            </section>
+        </main>
+    );
+}
+
+export default Error;
